ce0f2fd5e72536893978d10d710229ffdef0ca7c
DrMabuse23 <p.brewing@mwaysolutions.com>
2016-05-09 19:12:03 +0200

    fix env template

--- src/gii/templates/Environment.ts
+++ src/gii/templates/Environment.ts
@@ -1,13 +1,28 @@
 const Hjson = require('hjson');
 export class EnvironmentTemplate {
   public name:string = '';
+  /**
+   *   Hjson.parse(text, options)
 
+      options {
+        keepWsc     boolean, keep white space and comments. This is useful
+                    if you want to edit an hjson file and save it while
+                    preserving comments (default false)
+      }
+
+      This method parses Hjson text to produce an object or array.
+      It can throw a SyntaxError exception.
+
+
+    Hjson.stringify(value, options)
+
+   */
   get template():string {
     return Hjson.parse(`
-    {
-      //all vars are usable in your hjson files
-      name: ${this.name}
-    }`);
+{
+  //all vars are usable in your hjson files
+  name: ${this.name}
+}`,{keepWsc: true});
   }
 
   public render(name:string){
--- src/utility/FileApi.ts
+++ src/utility/FileApi.ts
@@ -6,10 +6,12 @@ export class FileApi {
   public encode:string = 'utf8';
   public hjsonSuffix: string = 'hjson';
   public path:string = `${__dirname}/../../devtest/`;
-
+  /**
+   * Hjson.stringify(value, options)
+   */
   writeHjson(content:string, fileName:string) {
     return Observable.create((observer:any) => {
-      fs.writeFile(`${this.path}${fileName}.${this.hjsonSuffix}`, Hjson.stringify(content), this.encode, (err:Error) => {
+      fs.writeFile(`${this.path}${fileName}.${this.hjsonSuffix}`, Hjson.stringify(content, {keepWsc: true}), this.encode, (err:Error) => {
         if (err) {
           return observer.error(err);
         }
