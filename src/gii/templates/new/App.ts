cf3e60a0b9831ff20f63cf2c75d5537030cd19c6
DrMabuse23 <p.brewing@mwaysolutions.com>
2016-06-07 14:14:32 +0200

    fix app.js ion new

--- src/gii/templates/new/App.ts
+++ src/gii/templates/new/App.ts
@@ -6,17 +6,30 @@ export class App implements TemplateInterface {
 
   get template(): string{
     return (html`
+      'use strict';
       /**
-       * ${this.name}
+       * @file app.js
+       * Simple MADP Application
        */
+      var Q = require('q');
+      Q.stopUnhandledRejectionTracking(); // requires use of Q's nextTick(cb)
+      Q.nextTick = process.nextTick;      // Q's nextTick(cb) is not compatible with thread-locals
+      //Q.longStackSupport = true;        // advanced diagnostic stack traces
+
       var express = require('express');
       var app = express();
-      // Assign middlewares
+
       app.use(express.bodyParser());
+
       // global variables
       global.app = app;
+
+      // install routes
+      require('./routes/routes');
+      require('./routes/connectors');
+
       // starts express webserver
-      app.listen();\n
+      app.listen();
     `);
   }
 }
