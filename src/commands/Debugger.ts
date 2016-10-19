557c329bb6523c03e4d80467629c88f373717983
Thomas Beckmann (M-Way) <t.beckmann@mwaysolutions.com>
2016-10-19 14:01:19 +0200

    fixes command line used to start debugger on Windows

--- src/commands/Debugger.ts
+++ src/commands/Debugger.ts
@@ -1,14 +1,14 @@
 import { Command } from './Command';
-import {ServerModelRc} from './../models/ServerModelRc';
-import {Deploy} from './project/Deploy';
-import {find} from 'lodash';
-import {FileApi} from '../utility/FileApi';
-import {RxFs} from './../utility/RxFs';
+import { ServerModelRc } from './../models/ServerModelRc';
+import { Deploy } from './project/Deploy';
+import { find } from 'lodash';
+import { FileApi } from '../utility/FileApi';
+import { RxFs } from './../utility/RxFs';
 import * as os from 'os';
 import * as path from 'path';
 import * as Relution from 'relution-sdk';
 
-import {Observable} from '@reactivex/rxjs';
+import { Observable } from '@reactivex/rxjs';
 
 export class Debugger extends Command {
 
@@ -50,46 +50,54 @@ export class Debugger extends Command {
      * get the relution.hjson
      */
     return this._fileApi.readHjson(path.join(this._deployCommand.projectDir, 'relution.hjson'))
-    /**
-     * get a server from inquirer
-     */
-    .exhaustMap((relutionHjson: { data: any, path: string }) => {
-      this._relutionHjson = relutionHjson.data;
-      return this._deployCommand.getServerPrompt();
-    })
-    .filter((server: { deployserver: string }) => {
-      return server.deployserver !== this.i18n.CANCEL;
-    })
-    /**
-     * logged in on server
-     */
-    .mergeMap((server: { deployserver: string }) => {
-      if (server.deployserver.toString().trim() === this._deployCommand.defaultServer.trim()) {
-        choosedServer = find(this.userRc.server, { default: true });
-      } else {
-        choosedServer = find(this.userRc.server, { id: server.deployserver });
-      }
-      return this.relutionSDK.login(choosedServer);
-    })
-    .mergeMap((resp: any) => {
-      const exec = require('child_process').exec;
-      const redirect = Relution.web.resolveUrl('node-inspector', { application: this._relutionHjson.name });
-      const url = `${choosedServer.serverUrl}/gofer/security-login?j_username=${choosedServer.userName}&j_password=${choosedServer.password}&redirect=${redirect}`;
-      let cmd = 'open --fresh';
-      if (os.platform() === 'win32') {
-        cmd = 'start';
-      }
-      return Observable.create((observer: any) => {
-        return exec(`${cmd} "${url}"`, (error: Error, stdout: any, stderr: any) => {
-          if (error) {
-            observer.error(`exec error: ${error}`);
-            return;
-          }
-          observer.next(`Debugger will open ${redirect} please wait`);
-          observer.complete();
+      /**
+       * get a server from inquirer
+       */
+      .exhaustMap((relutionHjson: { data: any, path: string }) => {
+        this._relutionHjson = relutionHjson.data;
+        return this._deployCommand.getServerPrompt();
+      })
+      .filter((server: { deployserver: string }) => {
+        return server.deployserver !== this.i18n.CANCEL;
+      })
+      /**
+       * logged in on server
+       */
+      .mergeMap((server: { deployserver: string }) => {
+        if (server.deployserver.toString().trim() === this._deployCommand.defaultServer.trim()) {
+          choosedServer = find(this.userRc.server, { default: true });
+        } else {
+          choosedServer = find(this.userRc.server, { id: server.deployserver });
+        }
+        return this.relutionSDK.login(choosedServer);
+      })
+      .mergeMap((resp: any) => {
+        const exec = require('child_process').exec;
+        const redirect = Relution.web.resolveUrl('node-inspector', { application: this._relutionHjson.name });
+        const url = `${choosedServer.serverUrl}/gofer/security-login?j_username=${
+          encodeURIComponent(choosedServer.userName)
+        }&j_password=${
+          encodeURIComponent(choosedServer.password)
+        }&redirect=${
+          encodeURIComponent(redirect)
+        }`;
+        let cmd: string;
+        if (os.platform() === 'win32') {
+          cmd = `${process.env.COMSPEC || 'cmd'} /C "@start /B ${url.replace(/&/g, '^&')}"`;
+        } else {
+          cmd = `open --fresh "${url}"`;
+        }
+        return Observable.create((observer: any) => {
+          return exec(cmd, (error: Error, stdout: any, stderr: any) => {
+            if (error) {
+              observer.error(`exec error: ${error}`);
+              return;
+            }
+            observer.next(`Debugger will open ${redirect} please wait`);
+            observer.complete();
+          });
         });
       });
-    });
   }
 }
 
